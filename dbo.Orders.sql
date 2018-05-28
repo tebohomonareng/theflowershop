9eda25a2f4dd3594028b04be172a1a95877a85cf
Teboho <7thbeatwave@gmail.com>
2018-05-28 13:04:00 +0200

    g

---
+++ dbo.Orders.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	[OrderID] INT IDENTITY(100,1) NOT NULL PRIMARY KEY,
+	[Email] VARCHAR(50) NOT NULL ,
+	[RecieverName] VARCHAR(250) NOT NULL,
+	[Address] VARCHAR(300) NOT NULL,
+	[Products] VARCHAR(250) NOT NULL, 
+	[TotalAmount] DECIMAL(6,2) NOT NULL,
+	[Province] VARCHAR(20) NOT NULL,
+	[OrderStatus] VARCHAR(20) DEFAULT 'Not delivered' NOT NULL
+)
