e746bfc3fa8c09afa285eeb48a06f1841e9274c4
tebohomonareng <38282426+tebohomonareng@users.noreply.github.com>
2018-05-29 13:25:26 +0200

    Update Customer.cs

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -14,7 +14,7 @@ namespace NetApp_Final_.Models
     
     public partial class Customer
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Customer()
         {
             this.CardInformations = new HashSet<CardInformation>();
